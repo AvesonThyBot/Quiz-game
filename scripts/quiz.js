aca3b21d90679c8c7f47e042e23d59c6ce88819e
Aveson <avesoncarvalho25@gmail.com>
2023-09-21 12:20:37 +0100

    Added script to toggle popup

---
+++ scripts/quiz.js
@@ -0,0 +1,25 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+
+
+// Start button
+startBtn.onclick= () => {
+    popupInfo.classList.add('active')
+    main.classList.add('active')
+
+} 
+
+// exit pop up button
+exitBtn.onclick= () => {
+    popupInfo.classList.remove('active')
+    main.classList.remove('active')
+
+} 
+
+// start quiz button
+continueBtn.onclick= () => {
+    popupInfo.classList.add('active')
+} 
